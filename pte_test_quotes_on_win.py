98adfaf565683126a9e8210f40e564e1619e202b
Christian Soeller <c.soeller@gmail.com>
2026-01-20 22:16:07 +0100

    tidying

--- pte_test_quotes_on_win.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-from subprocess import run
-
-proc = run(['mamba', "install", "--dry-run", 'setuptools<=73'],
-           text=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, check=True, shell=False) # we do not need or want a shell for install
-print(proc.stdout)
